8a2a0743333fbe5d95eda67efbdacac94a2f0c60
kajari1verma <kajari@simpledatalabs.com>
2019-04-01 13:43:14 +0530

    Failure test

---
+++ examples/slack.py
@@ -0,0 +1,37 @@
+start = DummyOperator(
+    task_id='start',
+    dag=dag)
+
+tmp_slack_test_dag = PostgresOperator(pool=redshift_pool,
+                                      task_id='tmp_slack_test_sql',
+                                      postgres_conn_id=redshift_conn_id,
+                                      sql="""sql/tmp_.sql""",
+                                      parameters=None,
+                                      autocommit=True,
+                                      dag=dag
+                                      )
+
+success_dummy = DummyOperator(
+    task_id='success_dummy',
+    dag=dag,
+    trigger_rule=TriggerRule.ALL_SUCCESS
+)
+
+alert_success_task = PythonOperator(
+    task_id='alert_success',
+    python_callable=lambda: send_slack(
+        senderRole='airflow',
+        receiverSubscribe='bot',
+        level='info',
+        text='success'+str(dt),
+        X_CAG_AUTH='AG_CONSUMER_TOKEN access-key=500000000000'
+    ),
+    #depends_on_past=True,
+    dag=dag
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag)
+
+start >> tmp_slack_test_dag >> success_dummy >> alert_success_task >> end
--- examples/spark_count_line.py
+++ examples/spark_count_line.py
@@ -5,10 +5,35 @@ from airflow.operators import PythonOperator
 
 from datetime import datetime
 
+from airflow.utils.email import send_email
+
+def notify_email(contextDict, **kwargs):
+    """Send custom email alerts."""
+
+    # email title.
+    title = "Airflow alert: {task_name} Failed".format(**contextDict)
+
+    # email contents
+    body = """
+    Hi Everyone, <br>
+    <br>
+    There's been an error in the {task_name} job.<br>
+    <br>
+    Forever yours,<br>
+    Airflow bot <br>
+    """.format(**contextDict)
+
+    send_email('you_email@address.com', title, body)
+
 args = {
     'owner': 'airflow'
     , 'start_date': datetime(2019, 4, 1)
     , 'provide_context': True
+    , 'email': "kajari@simpledatalabs.com"
+    , 'email_on_failure': True
+    , 'email_on_retry': False
+    , 'retries': 1
+    , 'retry_delay': timedelta(minutes=2),
 }
 
 dag = DAG(
@@ -29,4 +54,5 @@ t_main = PythonOperator(
     task_id = 'call_spark'
     , dag = dag
     , python_callable = run_spark
+   , on_failure_callback=notify_email
 )
