5d61061d58e602b733e51b4229f5a8d37c4ea6cf
kajari1verma <kajari@simpledatalabs.com>
2019-04-15 14:56:34 +0530

    Testing

--- examples/kube.py
+++ examples/kube.py
@@ -5,7 +5,6 @@ from airflow.operators.dummy_operator import DummyOperator
 from kubernetes import client, config
 
 config.load_incluster_config()
-config.load_kube_config()
 v1 = client.CoreV1Api()
 print("Listing pods with their IPs: kajari")
 ret = v1.list_pod_for_all_namespaces(watch=False)
