03abd4dfdfeed388d560eab507314179e58f704d
kajari1verma <kajari@simpledatalabs.com>
2019-04-16 09:58:29 +0530

    Check kube config contexts

--- examples/kube.py
+++ examples/kube.py
@@ -6,12 +6,12 @@ from kubernetes import client, config
 
 config.load_incluster_config()
 v1 = client.CoreV1Api()
-print("Listing pods with their IPs: kajari")
+print("Listing pods with their IPs: kajari1")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
     print("%s\t%s\t%s" %
           (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
+config.list_kube_config_contexts()
 
 default_args = {
     'owner': 'airflow',
