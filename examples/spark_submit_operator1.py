36237f846cafd933317eb894298f4d66ce668153
kajari1verma <35762598+kajari1verma@users.noreply.github.com>
2019-04-18 14:09:07 +0530

    test

--- examples/spark_submit_operator1.py
+++ examples/spark_submit_operator1.py
@@ -2,6 +2,7 @@ from airflow import DAG
 
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.utils import timezone
+from airflow.operators import PythonOperator
 
 
 DEFAULT_DATE = timezone.datetime(2019, 4, 17)
