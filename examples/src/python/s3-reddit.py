dce7ffce93fb22a09e689e4b611848fc9014d205
kajari1verma <kajari@simpledatalabs.com>
2019-04-22 14:16:21 +0530

    testing

--- examples/src/python/s3-reddit.py
+++ examples/src/python/s3-reddit.py
@@ -5,6 +5,6 @@ s3Bucket = sys.argv[1]
 s3Key = sys.argv[2]
 outfile = sys.argv[3]
 
-s3 = boto3.resource('s3', aws_access_key_id = 'AKIAITROTGC7VVQJOV7Q', aws_secret_access_key ='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')
+s3 = boto3.resource('s3', aws_access_key_id = '', aws_secret_access_key ='U')
 s3.meta.client.download_file(s3Bucket, s3Key, outfile)
 
